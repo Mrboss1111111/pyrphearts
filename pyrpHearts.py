62ed706dae6ecbbe5e53d556529233e1eb8979df
NJL <58279268+njlgbrl@users.noreply.github.com>
2021-04-02 19:29:23 +0800

    Added KH3 functionality

--- pyrpHearts.py
+++ pyrpHearts.py
@@ -13,12 +13,13 @@ from pypresence import Presence
 DETAILS = {'KH1': 'Kingdom Hearts 1.5 💙', 'KHCoM': 'Chain of Memories 🤍',
            'KH358': '358/2 Days ❤', 'KH2': 'Kingdom Hearts 2 💜',
            'KHBbS': 'Birth by Sleep 🧡', 'KHC': 'Coded 💛',
-           'KH3': 'Kingdom Hearts 3🖤'
+           'KH3': 'Kingdom Hearts 3 🖤'
            }
 PROGRAM_NAME = {'KH1': 'KINGDOM HEARTS FINAL MIX.exe',
                 'KHCoM': 'KINGDOM HEARTS Re_Chain of Memories.exe',
                 'KH2': 'KINGDOM HEARTS II FINAL MIX.exe',
                 'KHBbS': 'KINGDOM HEARTS Birth by Sleep FINAL MIX.exe',
+                'KH3': 'KINGDOM HEARTS III.exe',
                 'KHL': 'KINGDOM HEARTS HD 1.5+2.5 Launcher.exe',
                 'KHM': 'KINGDOM HEARTS HD 1.5+2.5 ReMIX.exe'
                 }
@@ -26,7 +27,7 @@ IMAGES = {'KH1': 'kh', 'KHCoM': 'khcom', 'KH2': 'kh2', 'KHBbS': 'kh1_5_2_5',
           'KHL': 'kh1_5_2_5', 'KH12': 'kh1_5_2_5', 'KH3': 'kh3', 'KHH': 'khh'
           }
 paths = {'KH12': f'{os.path.abspath(os.getcwd())}\KINGDOM HEARTS HD 1.5+2.5 ReMIX.exe',
-         'KH3': f'{os.path.abspath(os.getcwd())}\KINGDOM HEARTS 3.exe'}
+         'KH3': f'{os.path.abspath(os.getcwd())}\KINGDOM HEARTS III.exe'}
 
 logging.basicConfig(filename='pypr.log', filemode='w',
                     format='[%(asctime)s]%(levelname)s:\n%(message)s',
@@ -44,7 +45,7 @@ if config['Program']['KH12']:
     paths['KH3'] = config['Program']['KH3']
 
 state = "KINGDOM HEARTS 1.5 and 2.5 ReMIX" if config[
-    'State']['KH12'] == '<True>' else "KINGDOM HEARTS 3"
+    'State']['KH12'] == '<True>' else "KINGDOM HEARTS III"
 logging.info('Running pyrp')
 logging.info(f'State: {state} ')
 
@@ -126,6 +127,17 @@ try:
                        small_image=IMAGES['KHH'], small_text='Kingdom Hearts❣')
             logging.info('Presence updated! ')
             game_flags['KHBbS'] = True
+
+        # Kingdom Hearts III
+        if PROGRAM_NAME['KH3'] in (p.name() for p in psutil.process_iter()) and not game_flags['KH3']:
+            logging.info('KH3 detected!')
+            logging.info('Updating presence...')
+            start = time.time()
+            rpc.update(details=DETAILS['KH3'], start=start,
+                       large_image=IMAGES['KH3'], large_text='KH3',
+                       small_image=IMAGES['KHH'], small_text='Kingdom Hearts❣')
+            logging.info('Presence updated! ')
+            game_flags['KH3'] = True
 
         # Kingdom Hearts 1
         if PROGRAM_NAME['KH1'] not in (p.name() for p in psutil.process_iter()) and game_flags['KH1']:
@@ -283,7 +295,20 @@ try:
 
             game_flags['KHBbS'] = False
 
-            # Kingdom Hearts Launcher
+        # Kingdom Hearts III
+        if PROGRAM_NAME['KH3'] not in (p.name() for p in psutil.process_iter()) and game_flags['KH3']:
+            logging.warning('KH3 not detected anymore')
+            logging.info('Assuming game\'s no longer running')
+
+            if connect_flag:
+                rpc.close()
+
+            logging.info('Disconnecting presence')
+            logging.info('Exiting program...')
+            time.sleep(2)
+            break
+
+        # Kingdom Hearts Launcher
         if PROGRAM_NAME['KHL'] not in (p.name() for p in psutil.process_iter()) and game_flags['KHL'] and PROGRAM_NAME['KHM'] not in (p.name() for p in psutil.process_iter()):
             logging.warning('Launcher and main game not detected anymore')
 
